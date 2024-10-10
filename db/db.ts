5a77dadb63ac9c93b6b5d0b26403db3a7f67a84f
Everton Spader <everton.spader@gmail.com>
2024-10-10 13:25:20 +0200

    feat: connect to Neon database

--- db/db.ts
+++ db/db.ts
@@ -1,13 +1,12 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
-import * as schema from "./schema";
-import dotenv from "dotenv";
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+import { config } from "dotenv";
 
-dotenv.config();
+config({ path: ".env.local" }); // or .env.local
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL environment variable is not set");
 }
 
-export const client = postgres(process.env.DATABASE_URL);
-export const db = drizzle(client, { schema });
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle(sql);
