534418bded0ed6b14982c269dd58ed1d93b2d01c
Everton Spader <everton.spader@gmail.com>
2024-10-15 20:02:18 +0200

    chore: change button to toggle menu of sidebar

--- components/layout/main-layout.tsx
+++ components/layout/main-layout.tsx
@@ -40,40 +40,47 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
           isSidebarOpen ? "translate-x-0" : "-translate-x-full"
         } transition-transform duration-300 ease-in-out lg:relative lg:translate-x-0 ${
           isSidebarOpen ? "w-64" : "w-20"
-        }`}
+        } flex flex-col`}
       >
-        <div className="flex items-center justify-between h-20 shadow-md px-4">
+        <div className="flex items-center justify-between h-20 px-4">
           {isSidebarOpen && (
-            <h1 className="text-3xl font-bold text-gray-800">FinTracker</h1>
+            <h1 className="text-2xl font-bold text-gray-800">FinTracker</h1>
           )}
-          <button
-            onClick={toggleSidebar}
-            className="p-2 rounded-md bg-gray-200 text-gray-600 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-400"
-          >
-            {isSidebarOpen ? (
-              <ChevronLeft size={24} />
-            ) : (
-              <ChevronRight size={24} />
-            )}
-          </button>
         </div>
-        <nav className="mt-10">
-          {menuItems.map((item) => {
-            const Icon = item.icon;
-            return (
-              <Link
-                key={item.href}
-                href={item.href}
-                className={`flex items-center px-6 py-4 text-gray-700 hover:bg-gray-200 ${
-                  pathname === item.href ? "bg-gray-200" : ""
-                }`}
-              >
-                <Icon className="h-5 w-5 mr-3" />
-                {isSidebarOpen && item.label}
-              </Link>
-            );
-          })}
+        <nav className="flex-grow">
+          <div
+            className={`flex ${
+              isSidebarOpen ? "flex-col" : "flex-col items-center"
+            } space-y-2 mt-5`}
+          >
+            {menuItems.map((item) => {
+              const Icon = item.icon;
+              return (
+                <Link
+                  key={item.href}
+                  href={item.href}
+                  className={`flex items-center px-6 py-4 text-gray-700 hover:bg-gray-200 ${
+                    pathname === item.href ? "bg-gray-200" : ""
+                  } ${isSidebarOpen ? "justify-start" : "justify-center"}`}
+                  title={item.label}
+                >
+                  <Icon className="h-5 w-5" />
+                  {isSidebarOpen && <span className="ml-3">{item.label}</span>}
+                </Link>
+              );
+            })}
+          </div>
         </nav>
+        <button
+          onClick={toggleSidebar}
+          className="absolute bottom-5 right-0 p-2 bg-gray-200 text-gray-600 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-400 rounded-l-md"
+        >
+          {isSidebarOpen ? (
+            <ChevronLeft size={20} />
+          ) : (
+            <ChevronRight size={20} />
+          )}
+        </button>
       </div>
 
       {/* Main content */}
