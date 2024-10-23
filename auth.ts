11226b2a8fe39eb1ec45f0f85724c026ad07580f
Everton Spader <everton.spader@gmail.com>
2024-10-23 15:31:17 +0200

    feat: add auth and auth.config

---
+++ auth.ts
@@ -0,0 +1,56 @@
+import NextAuth, { type DefaultSession } from "next-auth";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { db } from "@/db";
+import authConfig from "@/auth.config";
+import { getUserById } from "@/db/queries";
+
+declare module "next-auth" {
+  interface Session {
+    user: DefaultSession["user"] & {
+      id: string;
+    };
+  }
+}
+
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth({
+  pages: {
+    signIn: "/signin",
+    // error: "/auth/error",
+  },
+  callbacks: {
+    async signIn({ user, account }) {
+      if (account?.provider !== "credentials") return true;
+
+      const existingUser = await getUserById(user.id!);
+
+      if (!existingUser) return false;
+
+      // TODO: Add any additional sign-in checks here if needed
+
+      return true;
+    },
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      return session;
+    },
+    async jwt({ token }) {
+      if (token?.sub) {
+        const existingUser = await getUserById(token.sub);
+        if (!existingUser) return token;
+        // Add any additional user data to the token if needed
+      }
+      return token;
+    },
+  },
+  adapter: DrizzleAdapter(db),
+  session: { strategy: "jwt" },
+  ...authConfig,
+});
