73c2f34dd69c97d0edb39f464182e46a372e57fb
Everton Spader <everton.spader@gmail.com>
2024-10-23 16:11:50 +0200

    chore: add get and generate token queries

--- db/queries.ts
+++ db/queries.ts
@@ -1,4 +1,7 @@
+import { v4 as uuidv4 } from "uuid";
+import { eq } from "drizzle-orm";
 import { db } from "@/db";
+import { verificationTokens } from "@/db/schema";
 
 export async function getUserByEmail(email: string) {
   return db.query.users.findFirst({
@@ -11,3 +14,49 @@ export async function getUserById(id: string) {
     where: (users, { eq }) => eq(users.id, id),
   });
 }
+
+export const getVerificationTokenByEmail = async (email: string) => {
+  try {
+    const verificationToken = await db.query.verificationTokens.findFirst({
+      where: eq(verificationTokens.email, email),
+    });
+    return verificationToken;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getVerificationTokenByToken = async (token: string) => {
+  try {
+    const verificationToken = await db.query.verificationTokens.findFirst({
+      where: eq(verificationTokens.token, token),
+    });
+    return verificationToken;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const generateVerificationToken = async (email: string) => {
+  const token = uuidv4();
+  const expires = new Date(new Date().getTime() + 3600 * 1000); // expires in one hour
+
+  const existingToken = await getVerificationTokenByEmail(email);
+
+  if (existingToken) {
+    await db
+      .delete(verificationTokens)
+      .where(eq(verificationTokens.id, existingToken.id));
+  }
+
+  const [verificationToken] = await db
+    .insert(verificationTokens)
+    .values({
+      email,
+      token,
+      expires,
+    })
+    .returning();
+
+  return verificationToken;
+};
