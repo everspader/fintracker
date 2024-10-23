8bf98e711dff0109d6e2fea143a938d80597fa85
Everton Spader <everton.spader@gmail.com>
2024-10-23 15:41:01 +0200

    fix: remove config(...) from db/index.ts

--- db/index.ts
+++ db/index.ts
@@ -1,9 +1,6 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
-import { config } from "dotenv";
 import * as schema from "./schema";
-
-config({ path: ".env.local" }); // or .env.local
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL environment variable is not set");
